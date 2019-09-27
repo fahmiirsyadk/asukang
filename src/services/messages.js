04b1a1425fc60a25ecb29a5de7670abb243949e2
fahmi irsyad khairi <fahmiirsyad10@gmail.com>
2019-09-27 11:41:48 +0700

    delete rxjs

--- src/services/messages.js
+++
@@ -1,9 +0,0 @@
-import { Subject } from "rxjs";
-
-const subject = new Subject();
-
-export const messageServices = {
-  sendMessage: message => subject.next(message),
-  clearMessage: () => subject.next(),
-  getMessage: () => subject.asObservable()
-};
