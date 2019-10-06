07ebe5dd81d975e3c760c42a21ad722fec346462
fahmi irsyad khairi <fahmiirsyad10@gmail.com>
2019-10-07 00:40:36 +0700

    Fix 0 nominal still show up
    TODO:
    - delete data when the nominal is Zero
    - add notification as Lunas

--- src/components/list-name/index.js
+++ src/components/list-name/index.js
@@ -6,12 +6,14 @@ import { listWrapper } from "./style";
 const ListName = ({ filteredName, action }) => {
   return (
     <div css={listWrapper}>
-      {filteredName.map((data, i) => (
-        <div key={i} onClick={() => action(data.name)}>
-          <h4>{data.name}</h4>
-          <p>{Math.abs(data.nominal)}</p>
-        </div>
-      ))}
+      {filteredName.map((data, i) => {
+        return data.nominal !== 0 ? (
+          <div key={i} onClick={() => action(data.name)}>
+            <h4>{data.name}</h4>
+            <p>{Math.abs(data.nominal)}</p>
+          </div>
+        ) : null;
+      })}
     </div>
   );
 };
